e4953be7225ee6cc3e4125413b90ee11bdbe9640
nicola <nicola.moro2312@gmail.com>
2022-11-13 02:49:35 +0100

    Implementation

--- main.go
+++ main.go
@@ -2,33 +2,65 @@ package main
 
 import "fmt"
 
-var BASE map[int]int = make(map[int]int)
+type Rectangle struct {
+	a []int
+	b []int
+	c []int
+	d []int
+	points [][]int
+}
 
-func countMapOccurencies(n int) (out int) {
-	for k, v := range BASE {
-		if k == n {
-			out += n
-		}
-		if BASE[k] == n {
-			out += BASE[v]
+func calculatePoints (A Rectangle) Rectangle {
+
+	A.points = append(A.points, A.a)
+	A.points = append(A.points, A.b)
+
+	for i:= A.a[0];i<A.b[0];i++ {
+		A.points = append(A.points,[]int{A.a[0]+1,A.a[1]})
+	}
+
+	for i,v := range A.points {
+		new := v
+		new[1] = i+2
+		if new[1] > A.d[1] {break}
+		A.points = append(A.points, new)
+	}
+	
+	return A
+}
+
+func countDuplicates (pointsA,pointsB [][]int) (out int) {
+	for _,v:= range pointsA {
+		for _,b := range pointsB {
+			if v == b {
+				//TODO: Fix here
+				out
+			}
 		}
 	}
-	return
 }
 
 func main() {
 
-	BASE[0] = 0
+	var A Rectangle = Rectangle {
+		a: []int{1,4},
+		b: []int{4,4},
+		c: []int{4,7},
+		d: []int{1,7},
+		points: [][]int{},
+	}
 
-	//T(1)
-	BASE[0] = countMapOccurencies(0)
-	BASE[1] = countMapOccurencies(1)
+	var B Rectangle = Rectangle {
+		a: []int{0,5},
+		b: []int{4,5},
+		c: []int{4,8},
+		d: []int{3,8},
+		points: [][]int{},
+	}
 
-	fmt.Println(BASE)
+		
 
-	//T(2)
-	BASE[0] = countMapOccurencies(0)
-	BASE[1] = countMapOccurencies(1)
-	BASE[2] = countMapOccurencies(2)
+	//fmt.Println(A.points)
+
 
 }
